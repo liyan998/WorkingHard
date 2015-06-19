a28003449461908db97bcedcc4d5332f8dbbbe7f
rein1 <lvxiongyi@126.com>
2015-06-19 14:54:08 +0800

    更新circle

--- unity-trench/Assets/Scripts/Manager/LoadingMgr.cs
+++ unity-trench/Assets/Scripts/Manager/LoadingMgr.cs
@@ -6,224 +6,204 @@ using System.Collections.Generic;
 public class LoadingMgr : MonoBehaviour
 {
 
-    public Slider LoadingSlider;
-    public Text LoadingText;
-    AsyncOperation async;
-    string strState = "";
-
-    [SerializeField]
-    bool IsUpdateRes = false;
-
-    [SerializeField]
-    TaskSlider mTaskSlider;
-
-    SingleDownMgr downMr;
-
-    [SerializeField]
-    GameObject mLoading;//loading菊花
-
+	public Slider LoadingSlider;
+	public Text LoadingText;
+	AsyncOperation async;
+	string strState = "";
+	[SerializeField]
+	bool
+		IsUpdateRes = false;
+	[SerializeField]
+	TaskSlider
+		mTaskSlider;
+	SingleDownMgr downMr;
+	[SerializeField]
+	LoadingCircle
+		mLoading;//loading菊花
 
 
-    //------------------------------------------
 
-    NetService mNetService;
+	//------------------------------------------
 
-	void Start() 
-    {
-        mNetService = GameManager.Instance.NetService;
+	NetService mNetService;
 
-        CheckNet();
+	void Start ()
+	{
+		mNetService = GameManager.Instance.NetService;
+
+		CheckNet ();
 	}
 
-    void CheckNet()
-    {
-        LoadingDlgManager.inst.HideDialog(0);
-        StartCoroutine(CheckNetWork());
-    }
+	void CheckNet ()
+	{
+		LoadingDlgManager.inst.HideDialog (0);
+		StartCoroutine (CheckNetWork ());
+	}
 
-    /// <summary>
-    /// 检查网络连接
-    /// </summary>
-    IEnumerator CheckNetWork()
-    {
-        if (Application.internetReachability == NetworkReachability.NotReachable)
-        {
-            Debug.Log("No Internet Conntion");
-            //弹窗提示
-            ShowDialogRetry();
-            yield return 0;
-        }
-        //登录服务器
-        //打开loading
+	/// <summary>
+	/// 检查网络连接
+	/// </summary>
+	IEnumerator CheckNetWork ()
+	{
+		if (Application.internetReachability == NetworkReachability.NotReachable) {
+			Debug.Log ("No Internet Conntion");
+			//弹窗提示
+			ShowDialogRetry ();
+			yield return 0;
+		}
+		//登录服务器
+		//打开loading
 
-        StartLoading(true);
+		StartLoading (true);
 
-        Debug.Log("Login Server Testing");
-        mNetService.LoginServer();
+		Debug.Log ("Login Server Testing");
+		mNetService.LoginServer ();
 
-        yield return new WaitForSeconds(2);
+		yield return new WaitForSeconds (2);
 
-        CallBackConntion(false);
-    }
+		CallBackConntion (false);
+	}
+
+	void StartLoading (bool sh)
+	{
+		mLoading.gameObject.SetActive (sh);
+		if (sh) {
+			mLoading.Roll ();
+		}
+	}
 
 
-    void StartLoading(bool sh)
-    {
-        mLoading.SetActive(sh);
-    }
-
-
-    /// <summary>
-    /// 连接响应回调
-    /// </summary>
-    void CallBackConntion(bool isSuccess)
-    {
-        //关闭loading
-        mLoading.SetActive(false);
-        if(isSuccess)
-        {
-            Debug.Log("Login Server successed!");
-            GameScene gs = GameManager.Instance.GameScene;
-            gs.SetGameScene(GameScene.SCENE.SCENE_NETSCENE_HALL);
-        }
-        else
-        {
-            Debug.Log("Login Server faild!");
-            ShowDialogRetry();
-        }       
-    }
+	/// <summary>
+	/// 连接响应回调
+	/// </summary>
+	void CallBackConntion (bool isSuccess)
+	{
+		//关闭loading
+		mLoading.gameObject.SetActive (false);
+		if (isSuccess) {
+			Debug.Log ("Login Server successed!");
+			GameScene gs = GameManager.Instance.GameScene;
+			gs.SetGameScene (GameScene.SCENE.SCENE_NETSCENE_HALL);
+		} else {
+			Debug.Log ("Login Server faild!");
+			ShowDialogRetry ();
+		}       
+	}
 
 
 
 
 
-    /// <summary>
-    /// 打开UI界面
-    /// </summary>
-    void ShowDialogRetry()
-    {        
-        Debug.Log("Open Dialog Retry!");
+	/// <summary>
+	/// 打开UI界面
+	/// </summary>
+	void ShowDialogRetry ()
+	{        
+		Debug.Log ("Open Dialog Retry!");
 
-        LoadingDlgManager.inst.ShowConfirmDialog("Retry描述", GoDownLobby, "去单机场", true, CheckNet, "重试");
-    }
-
-
-    /// <summary>
-    /// 进入单击场
-    /// </summary>
-    public void GoDownLobby()
-    {
-        LoadingDlgManager.inst.HideDialog(0);
-        if (IsUpdateRes)
-        {
-            List<string> wantdownGroup = new List<string>();
-            wantdownGroup.Add("testdown");
-            ResmgrNative.Instance.BeginInit("http://192.168.1.26/publicdown/", OnInitFinish, wantdownGroup);
-            LoadingText.text = "检查资源";
-        }
-        else
-        {
-            DataBase database = DataBase.Instance;
-            StartCoroutine(loadScence());
-        }
-    }
-    
-
-    bool indown = false;
-    void OnInitFinish(System.Exception err)
-    {
-        if (err == null)
-        {
-            ResmgrNative.Instance.taskState.Clear();
-            LoadingText.text = "检查资源完成";
-            List<string> wantdownGroup = new List<string>();
-            wantdownGroup.Add("testdown");
-            var downlist = ResmgrNative.Instance.GetNeedDownloadRes(wantdownGroup);
-            //判断需要下载的资源大小
-            if (ResmgrNative.Instance.taskState.tasksize > 0)
-            {
-                foreach (var d in downlist)
-                {
-                    d.Download(null);
-                }
-                ResmgrNative.Instance.WaitForTaskFinish(DownLoadFinish);
-                indown = true;
-            }
-            else 
-            {
-                LoadingText.text = "加载场景中...";
-                StartCoroutine(loadScence());
-            }            
-        }
-        else
-            strState = null;
-    }
+		LoadingDlgManager.inst.ShowConfirmDialog ("Retry描述", GoDownLobby, "去单机场", true, CheckNet, "重试");
+	}
 
 
-    void DownLoadFinish()
-    {
-        indown = false;
-        LoadingText.text = "更新完成";
-        LoadingSlider.value = 0;
-        mTaskSlider.SetPart(100);
-        LoadingText.text = "加载场景中...";
-        StartCoroutine(loadScence());
-    }
+	/// <summary>
+	/// 进入单击场
+	/// </summary>
+	public void GoDownLobby ()
+	{
+		LoadingDlgManager.inst.HideDialog (0);
+		if (IsUpdateRes) {
+			List<string> wantdownGroup = new List<string> ();
+			wantdownGroup.Add ("testdown");
+			ResmgrNative.Instance.BeginInit ("http://192.168.1.26/publicdown/", OnInitFinish, wantdownGroup);
+			LoadingText.text = "检查资源";
+		} else {
+			DataBase database = DataBase.Instance;
+			StartCoroutine (loadScence ());
+		}
+	}
 
-    //加载场景
-    IEnumerator loadScence()
-    {
-        async = Application.LoadLevelAsync("Lobby");
-        async.allowSceneActivation = false;
+	bool indown = false;
 
-        int displayProgress = 0;
-        int toProgress = 0;
-        while (async.progress < .9f)
-        {
-            toProgress = (int)(async.progress * 100);
+	void OnInitFinish (System.Exception err)
+	{
+		if (err == null) {
+			ResmgrNative.Instance.taskState.Clear ();
+			LoadingText.text = "检查资源完成";
+			List<string> wantdownGroup = new List<string> ();
+			wantdownGroup.Add ("testdown");
+			var downlist = ResmgrNative.Instance.GetNeedDownloadRes (wantdownGroup);
+			//判断需要下载的资源大小
+			if (ResmgrNative.Instance.taskState.tasksize > 0) {
+				foreach (var d in downlist) {
+					d.Download (null);
+				}
+				ResmgrNative.Instance.WaitForTaskFinish (DownLoadFinish);
+				indown = true;
+			} else {
+				LoadingText.text = "加载场景中...";
+				StartCoroutine (loadScence ());
+			}            
+		} else
+			strState = null;
+	}
 
-            while (displayProgress < toProgress)
-            {
-                displayProgress++;
-                LoadingSlider.value = displayProgress;
-                mTaskSlider.SetPart(displayProgress);
-                yield return new WaitForEndOfFrame();
-            }           
-        }
-        toProgress = 100;
-        while (displayProgress < toProgress)
-        {
-            displayProgress++;
-            LoadingSlider.value = displayProgress;
-            mTaskSlider.SetPart(displayProgress);
-            yield return new WaitForEndOfFrame();
-        }
+	void DownLoadFinish ()
+	{
+		indown = false;
+		LoadingText.text = "更新完成";
+		LoadingSlider.value = 0;
+		mTaskSlider.SetPart (100);
+		LoadingText.text = "加载场景中...";
+		StartCoroutine (loadScence ());
+	}
 
-        mTaskSlider.SetPart(toProgress);
-        LoadingText.text = "加载完成";
+	//加载场景
+	IEnumerator loadScence ()
+	{
+		async = Application.LoadLevelAsync ("Lobby");
+		async.allowSceneActivation = false;
+
+		int displayProgress = 0;
+		int toProgress = 0;
+		while (async.progress < .9f) {
+			toProgress = (int)(async.progress * 100);
+
+			while (displayProgress < toProgress) {
+				displayProgress++;
+				LoadingSlider.value = displayProgress;
+				mTaskSlider.SetPart (displayProgress);
+				yield return new WaitForEndOfFrame ();
+			}           
+		}
+		toProgress = 100;
+		while (displayProgress < toProgress) {
+			displayProgress++;
+			LoadingSlider.value = displayProgress;
+			mTaskSlider.SetPart (displayProgress);
+			yield return new WaitForEndOfFrame ();
+		}
+
+		mTaskSlider.SetPart (toProgress);
+		LoadingText.text = "加载完成";
 //        SOUND.Instance.SetMuteMode(false);
-        yield return new WaitForSeconds(0.2f);
-        async.allowSceneActivation = true;
-    }
+		yield return new WaitForSeconds (0.2f);
+		async.allowSceneActivation = true;
+	}
 
+	void Update ()
+	{
+		if (IsUpdateRes) {
+			ResmgrNative.Instance.Update ();
+			if (indown) {
+				float downingSize = ResmgrNative.Instance.taskState.downingSize / 1024f;
+				float tasksize = ResmgrNative.Instance.taskState.tasksize / 1024f;
 
+				LoadingText.text = downingSize.ToString ("f2") +
+					"M / " + downingSize.ToString ("f2") + "M";
+				LoadingSlider.value = downingSize / tasksize * 10;
 
-	void Update () 
-    {
-        if (IsUpdateRes)
-        {
-            ResmgrNative.Instance.Update();
-            if(indown)
-            {
-                float downingSize = ResmgrNative.Instance.taskState.downingSize / 1024f;
-                float tasksize = ResmgrNative.Instance.taskState.tasksize / 1024f;
-
-                LoadingText.text = downingSize.ToString("f2") +
-                                   "M / " + downingSize.ToString("f2") + "M";
-                LoadingSlider.value = downingSize / tasksize * 10;
-
-                mTaskSlider.SetPart((int)(LoadingSlider.value * 10));                
-            }
-        }
+				mTaskSlider.SetPart ((int)(LoadingSlider.value * 10));                
+			}
+		}
 	}
 }
